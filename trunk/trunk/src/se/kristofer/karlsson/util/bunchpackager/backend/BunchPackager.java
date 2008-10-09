911d7e4a6c639bf3ed640cc227b1a610d01d1284
kristofer.karlsson <kristofer.karlsson@990ad7be-9605-11dd-a1df-1f3f86a10fe5>
2008-10-09 13:44:19 +0000

    added gpl stuff

--- trunk/trunk/src/se/kristofer/karlsson/util/bunchpackager/backend/BunchPackager.java
+++ trunk/trunk/src/se/kristofer/karlsson/util/bunchpackager/backend/BunchPackager.java
@@ -1,3 +1,21 @@
+/*
+    Copyright (c) 2005, 2008 Kristofer Karlsson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 /**
  * Merges a collection of images onto a larger image, in such a way that the total
  * area is minimized. This problem is called 2d bin packing and is NP-complete.
