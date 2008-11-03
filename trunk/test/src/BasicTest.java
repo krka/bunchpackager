c55c5a067142b4782ba12e9a4c8c392d294cbfc4
kristofer.karlsson <kristofer.karlsson@990ad7be-9605-11dd-a1df-1f3f86a10fe5>
2008-11-03 18:46:43 +0000

    test stuff

---
+++ trunk/test/src/BasicTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.*;
+import static org.testng.AssertJUnit.*;
+
+@Test(enabled = true )
+public class BasicTest {
+	public void basicTest() {
+		assertTrue(true);
+	}
+}
+
